8e6bd0c8879238271e2f69cf018ba5a2a8a5a43a
Manish Ashok Kenjale <140141061+Manish230203@users.noreply.github.com>
2025-12-15 12:31:12 +0530

    Change app.run to use fixed port 5000

--- app.py
+++ app.py
@@ -157,5 +157,5 @@ def predict():
 # Start App
 # --------------------------------------
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    #port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=5000, debug=False)
